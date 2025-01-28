f862a638705c62b70bd863f1e1a045e9c1ab2cc1
tpietzsch <tobias.pietzsch@gmail.com>
2025-01-28 13:35:16 -0500

    WIP adapt n5 core changes

--- src/main/java/org/janelia/saalfeldlab/n5/zarr/ZarrStringDataBlock.java
+++ src/main/java/org/janelia/saalfeldlab/n5/zarr/ZarrStringDataBlock.java
@@ -12,59 +12,45 @@ public class ZarrStringDataBlock extends StringDataBlock {
         super(size, gridPosition, data);
     }
 
-    public ZarrStringDataBlock(int[] size, long[] gridPosition, byte[] data) {
-        super(size, gridPosition, data);
-    }
+	@Override
+	public byte[] serialize(final ByteOrder byteOrder) {
+		final int N = actualData.length;
+		final byte[][] encodedStrings = Arrays.stream(actualData).map((str) -> str.getBytes(ENCODING)).toArray(byte[][]::new);
+		final int[] lengths = Arrays.stream(encodedStrings).mapToInt((arr) -> arr.length).toArray();
+		final int totalLength = Arrays.stream(lengths).sum();
+		final ByteBuffer buf = ByteBuffer.wrap(new byte[totalLength + 4*N + 4]);
+		buf.order(byteOrder);
 
-    @Override
-    public void readData(final ByteBuffer buffer) {
-        try {
-            serializedData = buffer.array();
-        }
-        catch (UnsupportedOperationException e) { // buffer is a DirectByteBuffer -> copy contents
-            serializedData = new byte[buffer.limit()];
-            buffer.get(serializedData, 0, serializedData.length);
-        }
-        actualData = deserialize(serializedData);
-    }
+		buf.putInt(N);
+		for (int i = 0; i < N; ++i) {
+			buf.putInt(lengths[i]);
+			buf.put(encodedStrings[i]);
+		}
 
-    protected byte[] serialize(String[] strings) {
-        final int N = strings.length;
-        final byte[][] encodedStrings = Arrays.stream(strings).map((str) -> str.getBytes(ENCODING)).toArray(byte[][]::new);
-        final int[] lengths = Arrays.stream(encodedStrings).mapToInt((arr) -> arr.length).toArray();
-        final int totalLength = Arrays.stream(lengths).sum();
-        final ByteBuffer buf = ByteBuffer.wrap(new byte[totalLength + 4*N + 4]);
-        buf.order(ByteOrder.LITTLE_ENDIAN);
+		return buf.array();
+	}
 
-        buf.putInt(N);
-        for (int i = 0; i < N; ++i) {
-            buf.putInt(lengths[i]);
-            buf.put(encodedStrings[i]);
-        }
+	@Override
+	public void deserialize(final ByteOrder byteOrder, final byte[] serialized) {
+		serializedData = serialized;
 
-        return buf.array();
-    }
+		final ByteBuffer buf = ByteBuffer.wrap(serialized);
+		buf.order(byteOrder);
 
-    protected String[] deserialize(byte[] rawBytes) {
-        ByteBuffer buf = ByteBuffer.wrap(rawBytes);
-        buf.order(ByteOrder.LITTLE_ENDIAN);
+		// sanity check to avoid out of memory errors
+		if (serialized.length < 4)
+			throw new RuntimeException("Corrupt buffer, data seems truncated.");
 
-        // sanity check to avoid out of memory errors
-        if (rawBytes.length < 4)
-            throw new RuntimeException("Corrupt buffer, data seems truncated.");
+		final int n = buf.getInt();
+		if (serialized.length < n)
+			throw new RuntimeException("Corrupt buffer, data seems truncated.");
 
-        final int n = buf.getInt();
-        if (rawBytes.length < n)
-            throw new RuntimeException("Corrupt buffer, data seems truncated.");
-
-        final String[] strings = new String[n];
-        for (int i = 0; i < n; ++i) {
-            final int length = buf.getInt();
-            final byte[] encodedString = new byte[length];
-            buf.get(encodedString);
-            strings[i] = new String(encodedString, ENCODING);
-        }
-
-        return strings;
-    }
+		actualData = new String[n];
+		for (int i = 0; i < n; ++i) {
+			final int length = buf.getInt();
+			final byte[] encodedString = new byte[length];
+			buf.get(encodedString);
+			actualData[i] = new String(encodedString, ENCODING);
+		}
+	}
 }
