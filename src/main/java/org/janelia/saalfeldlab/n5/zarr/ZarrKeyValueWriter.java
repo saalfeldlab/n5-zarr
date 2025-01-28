90b95e822fe9f18adc713817164a66d31091e92b
tpietzsch <tobias.pietzsch@gmail.com>
2025-01-28 17:44:26 -0500

    revise padCrop

--- src/main/java/org/janelia/saalfeldlab/n5/zarr/ZarrKeyValueWriter.java
+++ src/main/java/org/janelia/saalfeldlab/n5/zarr/ZarrKeyValueWriter.java
@@ -41,14 +41,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import net.imglib2.Cursor;
-import net.imglib2.FinalInterval;
-import net.imglib2.img.array.ArrayImg;
-import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.img.basictypeaccess.array.ByteArray;
-import net.imglib2.type.numeric.integer.ByteType;
+import net.imglib2.blocks.SubArrayCopy;
 import net.imglib2.util.Intervals;
-import net.imglib2.view.Views;
 import org.janelia.saalfeldlab.n5.CachedGsonKeyValueN5Writer;
 import org.janelia.saalfeldlab.n5.Compression;
 import org.janelia.saalfeldlab.n5.DataBlock;
@@ -63,6 +57,8 @@ import org.janelia.saalfeldlab.n5.N5Exception.N5IOException;
 import org.janelia.saalfeldlab.n5.N5URI;
 import org.janelia.saalfeldlab.n5.RawCompression;
 import org.janelia.saalfeldlab.n5.cache.N5JsonCache;
+
+import static net.imglib2.util.Util.safeInt;
 
 /**
  * Zarr {@link GsonKeyValueN5Writer} implementation.
@@ -585,51 +581,38 @@ public class ZarrKeyValueWriter extends ZarrKeyValueReader implements CachedGson
 		final int n = srcBlockSize.length;
 
 		if (nBytes != 0) {
-			final int[] srcStrides = new int[n];
-			final int[] dstStrides = new int[n];
-			final int[] srcSkip = new int[n];
-			final int[] dstSkip = new int[n];
-			srcStrides[0] = dstStrides[0] = nBytes;
-			for (int d = 1; d < n; ++d) {
-				srcStrides[d] = srcBlockSize[d] * srcBlockSize[d - 1];
-				dstStrides[d] = dstBlockSize[d] * dstBlockSize[d - 1];
+			int[] zero = new int[n];
+			int[] srcSize = new int[n];
+			int[] dstSize = new int[n];
+			int[] size = new int[n];
+			Arrays.setAll(srcSize, d -> srcBlockSize[d] * (d == 0 ? nBytes : 1));
+			Arrays.setAll(dstSize, d -> dstBlockSize[d] * (d == 0 ? nBytes : 1));
+			Arrays.setAll(size, d -> Math.min(srcSize[d], dstSize[d]));
+			final byte[] dst = new byte[safeInt(Intervals.numElements(dstSize))];
+			if (!Arrays.equals(dstSize, size)) {
+				fill(dst, fill_value);
 			}
-			for (int d = 0; d < n; ++d) {
-				srcSkip[d] = Math.max(1, dstBlockSize[d] - srcBlockSize[d]);
-				dstSkip[d] = Math.max(1, srcBlockSize[d] - dstBlockSize[d]);
-			}
-
-			/* this is getting hairy, ImgLib2 alternative */
-			/* byte images with 0-dimension d[0] * nBytes */
-			final long[] srcIntervalDimensions = new long[n];
-			final long[] dstIntervalDimensions = new long[n];
-			srcIntervalDimensions[0] = srcBlockSize[0] * nBytes;
-			dstIntervalDimensions[0] = dstBlockSize[0] * nBytes;
-			for (int d = 1; d < n; ++d) {
-				srcIntervalDimensions[d] = srcBlockSize[d];
-				dstIntervalDimensions[d] = dstBlockSize[d];
-			}
-
-			final byte[] dst = new byte[(int)Intervals.numElements(dstIntervalDimensions)];
-			/* fill dst */
-			for (int i = 0, j = 0; i < n; ++i) {
-				dst[i] = fill_value[j];
-				if (++j == fill_value.length)
-					j = 0;
-			}
-			final ArrayImg<ByteType, ByteArray> srcImg = ArrayImgs.bytes(src, srcIntervalDimensions);
-			final ArrayImg<ByteType, ByteArray> dstImg = ArrayImgs.bytes(dst, dstIntervalDimensions);
-
-			final FinalInterval intersection = Intervals.intersect(srcImg, dstImg);
-			final Cursor<ByteType> srcCursor = Views.interval(srcImg, intersection).cursor();
-			final Cursor<ByteType> dstCursor = Views.interval(dstImg, intersection).cursor();
-			while (srcCursor.hasNext())
-				dstCursor.next().set(srcCursor.next());
-
+			SubArrayCopy.copy(src, srcSize, zero, dst, dstSize, zero, size);
 			return dst;
 		} else {
 			/* TODO deal with bit streams */
 			return null;
+		}
+	}
+
+	private static void fill(final byte[] dst, final byte[] fill_value) {
+		byte allZero = 0;
+		for (byte b : fill_value) {
+			allZero |= b;
+		}
+		if (allZero != 0) {
+			if (fill_value.length == 1) {
+				Arrays.fill(dst, fill_value[0]);
+			} else {
+				for (int i = 0; i < dst.length; i++) {
+					dst[i] = fill_value[i % fill_value.length];
+				}
+			}
 		}
 	}
 
