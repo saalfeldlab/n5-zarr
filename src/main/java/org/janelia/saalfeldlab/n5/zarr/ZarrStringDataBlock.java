f8d901cda8d0af46250b0634f9fae927d8b91954
tpietzsch <tobias.pietzsch@gmail.com>
2025-02-03 17:51:37 +0100

    WIP adapt n5 core changes

--- src/main/java/org/janelia/saalfeldlab/n5/zarr/ZarrStringDataBlock.java
+++ src/main/java/org/janelia/saalfeldlab/n5/zarr/ZarrStringDataBlock.java
@@ -1,10 +1,12 @@
 package org.janelia.saalfeldlab.n5.zarr;
 
+import java.io.IOException;
 import org.janelia.saalfeldlab.n5.StringDataBlock;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
+import org.janelia.saalfeldlab.n5.readdata.ReadData;
 
 public class ZarrStringDataBlock extends StringDataBlock {
 
@@ -13,7 +15,31 @@ public class ZarrStringDataBlock extends StringDataBlock {
     }
 
 	@Override
-	public byte[] serialize(final ByteOrder byteOrder) {
+	public void readData(final ReadData readData) throws IOException {
+		serializedData = readData.allBytes();
+
+		final ByteBuffer serialized = ByteBuffer.wrap(serializedData);
+		serialized.order(ByteOrder.LITTLE_ENDIAN);
+
+		// sanity check to avoid out of memory errors
+		if (serialized.limit() < 4)
+			throw new RuntimeException("Corrupt buffer, data seems truncated.");
+
+		final int n = serialized.getInt();
+		if (serialized.limit() < n)
+			throw new RuntimeException("Corrupt buffer, data seems truncated.");
+
+		actualData = new String[n];
+		for (int i = 0; i < n; ++i) {
+			final int length = serialized.getInt();
+			final byte[] encodedString = new byte[length];
+			serialized.get(encodedString);
+			actualData[i] = new String(encodedString, ENCODING);
+		}
+	}
+
+	@Override
+	public ReadData writeData(final ByteOrder byteOrder) {
 		final int N = actualData.length;
 		final byte[][] encodedStrings = Arrays.stream(actualData).map((str) -> str.getBytes(ENCODING)).toArray(byte[][]::new);
 		final int[] lengths = Arrays.stream(encodedStrings).mapToInt((arr) -> arr.length).toArray();
@@ -27,30 +53,6 @@ public class ZarrStringDataBlock extends StringDataBlock {
 			buf.put(encodedStrings[i]);
 		}
 
-		return buf.array();
-	}
-
-	@Override
-	public void deserialize(final ByteOrder byteOrder, final byte[] serialized) {
-		serializedData = serialized;
-
-		final ByteBuffer buf = ByteBuffer.wrap(serialized);
-		buf.order(ByteOrder.LITTLE_ENDIAN);
-
-		// sanity check to avoid out of memory errors
-		if (serialized.length < 4)
-			throw new RuntimeException("Corrupt buffer, data seems truncated.");
-
-		final int n = buf.getInt();
-		if (serialized.length < n)
-			throw new RuntimeException("Corrupt buffer, data seems truncated.");
-
-		actualData = new String[n];
-		for (int i = 0; i < n; ++i) {
-			final int length = buf.getInt();
-			final byte[] encodedString = new byte[length];
-			buf.get(encodedString);
-			actualData[i] = new String(encodedString, ENCODING);
-		}
+		return ReadData.from(buf);
 	}
 }
