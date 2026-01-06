709195b85ec4043ad3e43954b5317a1ebb967fe3
John Bogovic <bogovicj@janelia.hhmi.org>
2026-01-06 15:40:12 -0500

    test: ZarrShardTest is parameterized
    * minor clean up

--- src/test/java/org/janelia/saalfeldlab/n5/zarr/v3/ZarrShardTest.java
+++ src/test/java/org/janelia/saalfeldlab/n5/zarr/v3/ZarrShardTest.java
@@ -1,8 +1,6 @@
 package org.janelia.saalfeldlab.n5.zarr.v3;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 
@@ -13,11 +11,13 @@ import org.janelia.saalfeldlab.n5.N5FSTest;
 import org.janelia.saalfeldlab.n5.RawCompression;
 import org.janelia.saalfeldlab.n5.shard.ShardTest;
 import org.junit.After;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import com.google.gson.GsonBuilder;
 
 
-//@RunWith(Parameterized.class)
+@RunWith(Parameterized.class)
 public class ZarrShardTest extends ShardTest {
 
 	private static final boolean LOCAL_DEBUG = false;
@@ -27,15 +27,9 @@ public class ZarrShardTest extends ShardTest {
 		@Override public ZarrV3KeyValueWriter createTempN5Writer() {
 
 			final String basePath = new File(tempN5PathName()).toURI().normalize().getPath();
-			try {
-				String uri = new URI("file", null, basePath, null).toString();
-				return new ZarrV3KeyValueWriter(
-						new FileSystemKeyValueAccess(FileSystems.getDefault()),
-						basePath, new GsonBuilder(), false, true, "/", false);
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}
-			return null;
+			return new ZarrV3KeyValueWriter(
+					new FileSystemKeyValueAccess(FileSystems.getDefault()),
+					basePath, new GsonBuilder(), false, true, "/", false);
 		}
 
 		private String tempN5PathName() {
